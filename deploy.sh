aa02ef9535890d98065674efa6f3fa42605059aa
sfwang <fy800328@gmail.com>
2021-06-30 00:56:14 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,20 @@ set -e
 # build
 npm run build
 
-# push to remote repositorie
-git push -f git@github.com:fionasgithub/portfolio.git master
+# navigate into the build output directory
+cd src/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+# git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:fionasgithub/portfolio.git gh-pages
 
 cd -
